f75e38e133fe804aee0a5fc8d8638f3d819c8406
kde <kdeun1@naver.com>
2023-02-19 21:49:44 +0900

    axios 모듈 개발

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { setCommonInterceptors, setReqHeaders } from "./interceptors";
+
+const createApiInstance = () => {
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_APP_SERVER,
+  });
+  setCommonInterceptors(instance);
+  return instance;
+};
+
+const apiInstance = createApiInstance();
+
+export {
+  apiInstance,
+}
