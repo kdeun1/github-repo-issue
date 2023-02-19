9643173df9fde5e77573c8e268317b020a7a25ed
kde <kdeun1@naver.com>
2023-02-19 21:55:03 +0900

    feat: service layer 개발

---
+++ src/api/common/model.ts
@@ -0,0 +1,166 @@
+import { AxiosResponse } from "axios";
+
+type PromiseAxiosResponse<T> = Promise<AxiosResponse<T>>;
+type DateTime = string | null;
+
+interface Owner {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  received_events_url: string;
+  type: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  site_admin: boolean;
+}
+interface License {
+  key: string;
+  name: string;
+  url: string;
+  spdx_id: string;
+  node_id: string;
+  html_url: string;
+}
+/**
+ * A GitHub user.
+ */
+interface SimpleUser {
+  name?: string | null;
+  email?: string | null;
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string | null;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  starred_at?: string;
+  [k: string]: unknown;
+}
+interface Label {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  description: string;
+  color: string;
+  default: boolean;
+}
+interface Assignee {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_url: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+interface Creator {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+interface Milestone {
+  url: string;
+  html_url: string;
+  labels_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  state: string;
+  title: string;
+  description: string;
+  creator: Creator;
+  open_issues: number;
+  closed_issues: number;
+  created_at: DateTime;
+  updated_at: DateTime;
+  closed_at: DateTime;
+  due_on: string;
+}
+interface PullRequest {
+  url: string;
+  html_url: string;
+  diff_url: string;
+  patch_url: string;
+}
+interface ClosedBy {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+export type {
+  PromiseAxiosResponse,
+  DateTime,
+  Owner,
+  License,
+  SimpleUser,
+  Label,
+  Assignee,
+  Milestone,
+  PullRequest,
+  ClosedBy,
+}
